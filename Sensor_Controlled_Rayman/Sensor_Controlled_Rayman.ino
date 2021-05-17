94256b8595fdcc29426d162d71ce03a7a2cbc136
LiorRey <lior_rey@hotmail.com>
2021-05-17 22:40:08 +0300

    Final version
    Final version

--- Sensor_Controlled_Rayman/Sensor_Controlled_Rayman.ino
+++ Sensor_Controlled_Rayman/Sensor_Controlled_Rayman.ino
@@ -4,19 +4,17 @@
 #include <Keyboard.h>
 
 #define tapThreshold 120
-#define fistThreshold 5
+#define punchThreshold 5
+#define msTimeBeforeJump 10
 #define jumpThreshold 90
-#define airePressureLevel 10
 
 int lastMotionX = 0;
 int lastMotionY = 0;
 int lastMotionZ = 0;
-bool isChargingFist = false;
+bool isChargingPunch = false;
 bool isD7SwitchInitiallyOn = false;
 bool isOnCheatMode = false;;
-char masterMode[] = {KEY_TAB, 'p', 'r', 'o', 'g', 'r', 'a', 'm'};
-char tenMoreTings[] = {KEY_TAB, 'j', 'o', 'j', 'o'};
-char freeMovement[] = {KEY_TAB, ';', 'o', 'v', 'e', 'r', 'a', 'y', KEY_BACKSPACE};
+char freeMovement[] = {KEY_TAB, ';', 'o', 'v', 'e', 'r', 'a', 'y', KEY_BACKSPACE}; // [TAB];overay[BACKSPACE]
 
 void setup()
 {
@@ -36,14 +34,15 @@ void onTapHandler(void)
 
 void loop()
 {
-  getHorizontalInputs();
-  getFistInputs();
-  getJumpInputs();
+  handleMovement();
+  handlePunch();
+  handleJump();
   updateCheatMode();
   delay(100);
 }
 
-void getHorizontalInputs()
+// Handles Rayman's horizontal movement
+void handleMovement()
 {
   int keyPressed = -1;
 
@@ -62,20 +61,21 @@ void getHorizontalInputs()
     Keyboard.press(keyPressed);
     while (CircuitPlayground.leftButton() || CircuitPlayground.rightButton())
     {
-      // allow actions while moving
-      getFistInputs();
-      getJumpInputs();
+      // Allows actions while moving
+      handlePunch();
+      handleJump();
     }
+
     Keyboard.release(keyPressed);
   }
 }
 
-void getFistInputs()
+// Samples the accelerometer in every 3D-axis and checks if a punch should be charged and launched according to the samplings
+void handlePunch()
 {
   int motionX = CircuitPlayground.motionX();
   int motionY = CircuitPlayground.motionY();
   int motionZ = CircuitPlayground.motionZ();
-
   int deltaX = abs(motionX - lastMotionX);
   int deltaY = abs(motionY - lastMotionY);
   int deltaZ = abs(motionZ - lastMotionZ);
@@ -84,23 +84,22 @@ void getFistInputs()
   lastMotionX = motionX;
   lastMotionY = motionY;
   lastMotionZ = motionZ;
-
-  if (overallDelta > fistThreshold && !isChargingFist)
+  if (overallDelta > punchThreshold && !isChargingPunch)
   {
-    isChargingFist = true;
+    isChargingPunch = true;
     Keyboard.press(KEY_LEFT_ALT);
   }
-
-  else if (overallDelta < fistThreshold)
+  else if (overallDelta < punchThreshold)
   {
-    isChargingFist = false;
+    isChargingPunch = false;
     Keyboard.release(KEY_LEFT_ALT);
   }
 }
 
-void getJumpInputs()
+// Checks if the sound pressure level of the microphone is greater than the given jump threshold, if so - perform a jump
+void handleJump()
 {
-  if (CircuitPlayground.mic.soundPressureLevel(airePressureLevel) > jumpThreshold)
+  if (CircuitPlayground.mic.soundPressureLevel(msTimeBeforeJump) > jumpThreshold)
   {
     Keyboard.press(KEY_LEFT_CTRL);
   }
@@ -110,6 +109,7 @@ void getJumpInputs()
   }
 }
 
+// Checks if the slide switch has been toggled relative to its initial state, to enable/disable the cheat mode accordingly
 void updateCheatMode()
 {
   if (CircuitPlayground.slideSwitch() && !isOnCheatMode)
@@ -118,7 +118,6 @@ void updateCheatMode()
     isOnCheatMode = true;
     switchFreeMovementCheat();
   }
-
   else if (!CircuitPlayground.slideSwitch() && isOnCheatMode)
   {
     isOnCheatMode = false;
@@ -126,30 +125,12 @@ void updateCheatMode()
   }
 }
 
-void masterModeCheat()
-{
-  for (int i = 0; i < strlen(masterMode); i++)
-    {
-      char c = masterMode[i];
-      Keyboard.write(c);
-    }
-}
-
-void tenMoreTingsCheat()
-{
-  for (int i = 0; i < strlen(tenMoreTings); i++)
-    {
-      char c = tenMoreTings[i];
-      Keyboard.write(c);
-    }
-}
-
-// Toggle on/off the cheat of free movement: "[TAB];overay[BACKSPACE]"
+// Toggles on/off the cheat of free movement: "[TAB];overay[BACKSPACE]"
 void switchFreeMovementCheat()
 {
-    for (int i = 0; i < strlen(freeMovement); i++)
-    {
-      char c = freeMovement[i];
-      Keyboard.write(c);
-    }
+  for (int i = 0; i < strlen(freeMovement); i++)
+  {
+    char c = freeMovement[i];
+    Keyboard.write(c);
+  }
 }
